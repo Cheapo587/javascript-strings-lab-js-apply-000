63cad75454492b858590af87c303037bf5af86e2
Alex S <ac.shultz587@gmail.com>
2017-10-11 17:54:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!"
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = `Hello, ${special guest}!`
+var greetSpecialGuest = `Hello, ${specialGuest}!`
 
 var topic = "Let's talk about space."
 
